5da7110210439a5b8dd972c37dcabcb7ab595569
João Victor <jvcgomes14@gmail.com>
2025-09-22 11:20:28 -0300

    fixing minor errors

--- app/__init__.py
+++ app/__init__.py
@@ -2,17 +2,21 @@ from flask import Flask, session, request, current_app
 from config import Config
 from flask_babel import Babel
 
-babel = Babel
+def get_locale():
+    if 'language' in session:
+        return session['language']
+    return request.accept_languages.best_match(current_app.config['LANGUAGES'])
+
+babel = Babel()
 
 def create_app(config_class=Config):
 
     # Creates the main application instance.
     app = Flask(__name__)
-
-    babel.init_app(app)
-
     # Loads the configuration from the object we passed (config.Config).
     app.config.from_object(config_class)
+
+    babel.init_app(app, locale_selector=get_locale)
 
     # Here is where we will register our Blueprints.
     # Blueprints are like mini-applications that help organize routes.
@@ -21,8 +25,3 @@ def create_app(config_class=Config):
 
     # Returns the created and configured application instance.
     return app
-
-def get_locale():
-    if 'language' in session:
-        return session['language']
-    return request.accept_languages.best_match(current_app.config['LANGUAGES'])
